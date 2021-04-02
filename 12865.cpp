539c4240cafd737c44a72234c4fe026fe7600c1f
박근형 <52066828+w8385@users.noreply.github.com>
2021-04-03 08:33:17 +0900

    Add files via upload

---
+++ 12865.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int check(int n, int count){
+	n >> count;
+	if(n == 1) return 1;
+	else return 0;
+}
+int main(){
+	int N, K;
+	int arr[2][105] = {};
+	scanf("%d%d",&N,&K);
+	for(int i = 0; i < N; i++)
+		scanf("%d%D",&arr[0][i],&arr[1][i]);
+	}
+	int cur = 1;
+	while(cur++){
+		for(int i = 0; i < N; i++){
+			
+			
+}
